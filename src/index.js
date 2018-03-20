ff7094e9da18feba76d1f71d510b4e5c74e5cde0
Ian Dewsbury <ianmichaeld@gmail.com>
2018-03-20 11:44:00 -0400

    added a map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('/src/index.js is running!');
+
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaWFubWljaGFlbGQiLCJhIjoiY2plenRsdTJ5MGU4bzJ4bnZqYzh6Nng2OSJ9.kN8zSDJhvCrwuSgj894Pvw';
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
